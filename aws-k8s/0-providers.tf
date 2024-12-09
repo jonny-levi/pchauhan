3d54888478e495d60d7647dfe8ed2c2465b0f478
jonathan <yonilevi0013@gmail.com>
2024-12-09 23:12:55 +0200

    helm wip

--- aws-k8s/0-providers.tf
+++ aws-k8s/0-providers.tf
@@ -8,8 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region     = "us-east-1"
+  profile    = "default"
+  access_key = "AKIAZTHXXYEGEZUUMUYA"
+  secret_key = "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"
 }
 
 
